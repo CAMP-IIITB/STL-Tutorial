07fe77a17e8067ccf29ef1d861074326da96232d
Rathin Bhargava <30757416+Mindjolt2406@users.noreply.github.com>
2020-03-24 23:48:16 +0530

    Update sorting.cpp

--- sorting.cpp
+++ sorting.cpp
@@ -1,8 +1,15 @@
 #include <cstdio>
 #include <vector>
 #include <algorithm>
+#include<pair> 
 
 using namespace std;
+
+bool comp(pair<int,int> &p1, pair<int,int> &p2>
+{
+  if(p1.first > p2.first && p1.second < p2.second) return true;
+  return false;
+}
 
 int main(){
   // a program that demonstrates how to use C++'s inbuilt sort function to sort arrays and vectors
@@ -33,6 +40,13 @@ int main(){
   for(int i = 0; i < n; i++){
      printf("vec[%d] = %d\n", i, vec[i]);
   }
+  
+  // We can also use the inbuilt sort function to sort based on custom comparators
+  // Let's define a vector of pairs, where we would like to sort based on the first element in descending order
+  // and on the second in increasing order
+  // We have defined comp above
+  vector<pair<int,int> , comp > w;
+  // Remember a comparator should return true only if x < y. Not x <= y. 
   
   return 0;
 }
